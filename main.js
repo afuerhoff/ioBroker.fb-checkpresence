9880826db657c0dc0c36afa30b523a7f6aa45866
afuerhoff <50245893+afuerhoff@users.noreply.github.com>
2025-02-08 21:40:26 +0100

    externalIPv6

--- main.js
+++ main.js
@@ -275,6 +275,7 @@ class FbCheckpresence extends utils.Adapter {
                     cnt2 = 0;
                     if (this.config.extip === true) {
                         await this.setStateChangedAsync('info.extIp', { val: await this.Fb.getExtIp(), ack: true });
+                        await this.setStateChangedAsync('info.extIpv6', { val: await this.Fb.getExtIpv6(), ack: true });
                     }
                     if (this.config.guestinfo === true) {
                         await this.setStateChangedAsync('guest.wlan', { val: await this.Fb.getGuestWlan(), ack: true });
